dbdaf993d5c3c03fdec0ab0c002040bea7967ff5
root <root@EXOCORE-PC.localdomain>
2026-08-27 20:57:32 +0100

    Set base path to /airspace/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/airspace/',
   server: {
     proxy: {
       // Proxy ADS-B lol API to avoid CORS issues
